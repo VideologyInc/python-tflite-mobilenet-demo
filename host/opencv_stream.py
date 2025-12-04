97225f3d6ce4214c4d610cacb5f25ad2795dfcd6
PingVideology <jye@videologyinc.com>
2025-12-04 14:27:17 -0500

    AB#1783. To test object detection py (no Hailo version) in 2 Scailx cameras at the same time, modified ~/host/opencv_stream.py to support argparse to specify camera 1 or 2.

--- host/opencv_stream.py
+++ host/opencv_stream.py
@@ -1,9 +1,10 @@
-
+import argparse
 import cv2
 
 # To see whether opencv has gstreamer support.
 # Need to rebuild opencv from source if not (both on Windows and on Linux).
 # print(cv2.getBuildInformation())
+
 
 def stream_loop(cap):
     while True:
@@ -16,31 +17,49 @@ def stream_loop(cap):
         cv2.imshow("Scailx RTSP Stream", frame)
 
         # Press 'q' to exit the stream
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-    
 
-# Replace with your camera's RTSP URL. 
-# This often includes username, password, IP address, port, and stream path.
 
-scailx_rtsp_url = "rtsp://scailx-ai.local:8554/stream" # cam0-gs-AR0234_1080p" 
-# ip201_rtsp_url = "rtsp://10.195.53.158/rtsp_stream_00"
-# ip201_rtsp_url = "rtsp://192.168.0.111/rtsp_stream_00"
-
+# gstreamer src is not supported for now ;-)
+"""
 gstreamer_pipeline = (
     "rtspsrc location=rtsp://scailx-ai.local:8554/stream latency=0 connection-speed=3000 ! "
     "queue ! decodebin ! queue ! videoconvert ! autovideosink sync=false"
 )
-
+"""
 # cap2 = cv2.VideoCapture(gstreamer_pipeline, cv2.CAP_GSTREAMER)
-cap2 = cv2.VideoCapture(scailx_rtsp_url)
 
-if not cap2.isOpened():
-    print("Error: Could not open RTSP stream.")
-    exit()
+if __name__ == "__main__":
 
-stream_loop(cap2)
+    parser = argparse.ArgumentParser(
+        description="Camera Object Detection Test", prog="opencv_stream"
+    )
 
-cap2.release()
+    parser.add_argument(
+        "-i",
+        "--input",
+        default=1,
+        type=int,
+        help="Scailx camera: 1 = scailx-ai or 2 = scailx-ai-2",
+    )
 
-cv2.destroyAllWindows()
+    args = parser.parse_args()
+
+    scailx_rtsp_url = (
+        "rtsp://scailx-ai.local:8554/stream"
+        if args.input == 1
+        else "rtsp://scailx-ai-2.local:8554/stream"
+    )
+
+    cap = cv2.VideoCapture(scailx_rtsp_url)
+
+    if not cap.isOpened():
+        print("Error: Could not open RTSP stream.")
+        exit()
+
+    stream_loop(cap)
+
+    cap.release()
+
+    cv2.destroyAllWindows()
