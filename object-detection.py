6bbf0809a6910d2941f95e4b4ed7e19da5345312
PingVideology <jye@videologyinc.com>
2026-01-29 13:18:22 -0500

    AB#1946. Minor patch on gst string.

--- object-detection.py
+++ object-detection.py
@@ -144,16 +144,16 @@ class InferenceDataFactory(GstRtspServer.RTSPMediaFactory):
         self.duration = (1.0 / CAPTURE_FRAMERATE) * Gst.SECOND  # duration of a frame in nanoseconds
 
         # Create opencv Video Capture
-        self.cap = cv2.VideoCapture(f'v4l2src device={DEVICE} extra-controls="controls,horizontal_flip=0,vertical_flip=0" ' \
-                                    f'! video/x-raw,width={CAPTURE_RESOLUTION_X},height={CAPTURE_RESOLUTION_Y},framerate={CAPTURE_FRAMERATE}/1 ' \
-                                    f'! imxvideoconvert_g2d ' \
+        self.cap = cv2.VideoCapture(f'v4l2src device={DEVICE} ' \
+                                    f'! video/x-raw,width={CAPTURE_RESOLUTION_X},height={CAPTURE_RESOLUTION_Y},framerate={CAPTURE_FRAMERATE}/1,format=NV12 ' \
+                                    f'! queue ! imxvideoconvert_g2d ' \
                                     f'! video/x-raw,format=BGRA ' \
                                     f'! appsink', cv2.CAP_GSTREAMER)
 
         # Create factory launch string
         self.launch_string = f'appsrc name=source is-live=true format=GST_FORMAT_TIME ' \
                              f'! video/x-raw,format=BGRA,width={CAPTURE_RESOLUTION_X},height={CAPTURE_RESOLUTION_Y},framerate={CAPTURE_FRAMERATE}/1 ' \
-                             f'! vpuenc_h264 bitrate={STREAM_BITRATE} ' \
+                             f'! queue ! vpuenc_h264 bitrate={STREAM_BITRATE} ' \
                              f'! rtph264pay config-interval=1 name=pay0 pt=96 '
 
           
